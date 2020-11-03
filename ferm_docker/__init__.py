a2ca7959a97fd7976c1e9a1abf1a9ae34d452a2d
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-11-03 16:07:45 +0100

    Add incomplete docker swarm support
    Specifically adds support for
    - port mappings on services
    - the DOCKER-INGRESS chain
    - enabling/disabling rules when swarm is enabled/disabled
    
    But this isn't finished yet, but I still think we need the better
    network support that is part of this change.
    
    Relates-to: #4

--- ferm_docker/__init__.py
+++ ferm_docker/__init__.py
@@ -8,7 +8,8 @@ __version__ = "0.1.1"
 
 client = docker.APIClient()
 
-def get_nested(dct, *keys):
+
+def get_nested(dct: dict, *keys: list):
     for key in keys:
         try:
             dct = dct[key]
@@ -18,11 +19,18 @@ def get_nested(dct, *keys):
 
 
 def get_network_by_id(network_id: str) -> dict:
-    networks = client.networks()
+    for item in client.networks():
+        if item.get("Id") == network_id:
+            return Network(item)
 
-    for network in networks:
-        if network.get("Id") == network_id:
-            return Network(network)
+
+def get_service_by_id(service_id: str) -> dict:
+    try:
+        for item in client.services():
+            if item.get("Id") == service_id:
+                return Service(item)
+    except docker.errors.APIError:
+        return
 
 
 class Network:
@@ -30,7 +38,7 @@ class Network:
         self._network = network
 
     @property
-    def _id(self):
+    def id(self):
         return self._network["Id"]
 
     @property
@@ -39,11 +47,49 @@ class Network:
 
     @property
     def ifname(self):
-        return get_nested(self._network, "Options", "com.docker.network.bridge.name") or f"br-{self._id[:12]}"
+        return (
+            get_nested(self._network, "Options", "com.docker.network.bridge.name")
+            or f"br-{self.id[:12]}"
+        )
+
+    @property
+    def ingress(self):
+        # used to mark public interfaces in docker swarm setups
+        return self._network["Ingress"]
+
+    @property
+    def ip_masquerade(self):
+        return (
+            get_nested(
+                self._network,
+                "Options",
+                "com.docker.network.bridge.enable_ip_masquerade",
+            )
+            or False
+        )
 
     @property
     def prefix(self):
-        return get_nested(self._network, "IPAM", "Config").pop()["Subnet"]
+        ipam_config = get_nested(self._network, "IPAM", "Config")
+        if not ipam_config:
+            return False
+
+        return ipam_config.pop()["Subnet"]
+
+    def inspect(self):
+        return client.inspect_network(self.id)
+
+
+class Service:
+    def __init__(self, service):
+        self._service = service
+
+    @property
+    def port_mappings(self):
+        return [
+            SwarmPortMapping(mapping, self)
+            for mapping in get_nested(self._service, "Endpoint", "Ports")
+        ]
 
 
 class PortMapping:
@@ -64,31 +110,79 @@ class PortMapping:
         return self._mapping["PublicPort"]
 
 
+class SwarmPortMapping(PortMapping):
+    @property
+    def proto(self):
+        return self._mapping["Protocol"]
+
+    @property
+    def internal_port(self):
+        return self._mapping["TargetPort"]
+
+    @property
+    def external_port(self):
+        return self._mapping["PublishedPort"]
+
+    @property
+    def network(self):
+        return self._container.networks[self._mapping["PublishMode"]]
+
+
 class Container:
     def __init__(self, container):
         self._container = container
 
-        self.network = get_network_by_id(self.network_id)
+        self.networks = {
+            name: get_network_by_id(network.get("NetworkID"))
+            for name, network in get_nested(
+                container, "NetworkSettings", "Networks"
+            ).items()
+        }
+        self.service = get_service_by_id(
+            get_nested(self._container, "Labels", "com.docker.swarm.service_id")
+        )
+
+        self.swarm = self.service is not None
 
     @property
-    def network_name(self):
-        return get_nested(self._container, "HostConfig", "NetworkMode")
+    def id(self):
+        return self._container["Id"]
 
     @property
-    def network_id(self):
-        network_id = get_nested(self._container, "NetworkSettings", "Networks", self.network_name, "NetworkID")
-        assert network_id is not None
-        return network_id
+    def ingress_networks(self):
+        if self.swarm:
+            return [network for network in self.networks.values() if network.ingress]
+
+        return [
+            network for network in self.networks.values() if network.driver == "bridge"
+        ]
 
     @property
     def ip(self):
-        return get_nested(self._container, "NetworkSettings", "Networks", self.network_name, "IPAddress")
+        addrs = []
+
+        for network in self.ingress_networks:
+            inspection = network.inspect()
+
+            if self.id in inspection["Containers"]:
+                addrs.append(
+                    get_nested(inspection, "Containers", self.id, "IPv4Address").split(
+                        "/"
+                    )[0]
+                )
+
+        return addrs.pop()
 
     @property
     def port_mappings(self):
-        required_keys = ["Type", "PrivatePort", "PublicPort"]
-        return [PortMapping(mapping, self) for mapping in self._container["Ports"] if all([key in mapping for key in required_keys])]
+        if self.swarm:
+            return self.service.port_mappings
 
+        return [
+            PortMapping(mapping, self)
+            for mapping in self._container.get("Ports", [])
+            if "PublicPort" in mapping
+        ]
 
 
 def main():
@@ -97,25 +191,31 @@ def main():
             dump()
             sys.exit(0)
 
-    env = Environment(
-         loader=PackageLoader('ferm_docker')
-    )
+    env = Environment(loader=PackageLoader("ferm_docker"))
 
     template = env.get_template("ferm.j2")
 
-    print(template.render(
-        containers=list(map(Container, client.containers())),
-        networks=list(map(Network, client.networks()))
-    ))
+    # test for docker swarm
+    try:
+        client.services()
+        swarm = True
+    except docker.errors.APIError:
+        # 503 Server Error: Service Unavailable ("This node is not a swarm manager. Use "docker swarm init" or "docker swarm join" to connect this node to swarm and try again.")
+        swarm = False
+
+    print(
+        template.render(
+            containers=list(map(Container, client.containers())),
+            networks=list(map(Network, client.networks())),
+            swarm=swarm,
+        )
+    )
 
 
 def dump():
-    for response in [
-            client.containers(),
-            client.networks()]:
+    for response in [client.containers(), client.networks()]:
         print(json.dumps(response, indent=2))
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
